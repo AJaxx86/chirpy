ad216f35a8422b057248bbc282f4c3be0a19223f
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2026-08-11 16:16:07 +0100

    ch2 done

--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 
 func main() {
+	apiCfg := apiConfig{}
 	router := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
-
+	
 	handler := http.FileServer(http.Dir("."))
-	router.Handle("/app/", http.StripPrefix("/app", handler))
-	router.HandleFunc("/healthz", readinessHandler)
+	router.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", handler)))
+	router.HandleFunc("GET /healthz", handlerReadiness)
+	router.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	router.HandleFunc("POST /reset", apiCfg.handlerMetricsReset)
 	server.ListenAndServe()
 }
 
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 
@@ -29,4 +37,38 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
+}
+
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	visits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	body := []byte(fmt.Sprintf("Hits: %v", visits))
+	_, err := w.Write(body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+}
+
+
+func (cfg *apiConfig) handlerMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Swap(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	body := []byte(fmt.Sprintf("Hits: %v", 0))
+	_, err := w.Write(body)
+	if err != nil {
+		fmt.Sprintf("%s", err)
+	}
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
