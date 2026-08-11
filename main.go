ea23d7523994182aa6f9a107b7dcbbb21f937fe5
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2026-08-11 15:32:58 +0100

    ch1 done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
 
@@ -11,5 +12,21 @@ func main() {
 		Addr:    ":8080",
 		Handler: router,
 	}
+
+	handler := http.FileServer(http.Dir("."))
+	router.Handle("/app/", http.StripPrefix("/app", handler))
+	router.HandleFunc("/healthz", readinessHandler)
 	server.ListenAndServe()
+}
+
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	body := []byte("OK")
+	_, err := w.Write(body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 }
